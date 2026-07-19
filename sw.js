fdac87b0ebcff7e63bc4b0e492e77edfd1272e43
yjslifeinbeta <liuyanjunlaila@gmail.com>
2026-07-19 22:31:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="chuanma-score-v8-3";
+const CACHE="chuanma-score-v8-4";
 const FILES=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
