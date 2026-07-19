37e7c7d336ff5078c2b0cd1e2c428d7289074b90
liuyanjunlaila-wq <liuyanjunlaila@gmail.com>
2026-07-19 20:21:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="chuanma-score-v8-3";
+const FILES=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
